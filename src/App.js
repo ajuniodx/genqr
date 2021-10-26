0f70178505f4b9569ec6236c1bbbd8b91e14865a
alex junio <alexjunioferreira17@gmail.com>
2021-10-26 13:21:47 -0300

    init layout app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import QenQr from "./genQr";
 
 function App() {
+  const [text, setText] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <header className="container">
+        <a className="gen-logo" href="/">
+          QenQr
+        </a>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className="btn-outline"
+          target="blank"
+          href="https://www.paypal.com/donate?business=AKNFF9KV6L98Q&no_recurring=0&currency_code=BRL"
         >
-          Learn React
+          Doar para o projeto
         </a>
       </header>
+      <section>
+        <div className="container">
+          <div className="content">
+            <div className="qr-section">
+              <div className="qr-content">
+                <QenQr className="gen-code" text={text}></QenQr>
+              </div>
+              <p>Não armazenamos nenhum dado informado.</p>
+              <div className="forms">
+                <input
+                  type="text"
+                  placeholder="Link do QR Code"
+                  onChange={(element) => {
+                    setText(element.target.value);
+                  }}
+                  value={text}
+                ></input>
+              </div>
+            </div>
+            <div className="qr-about">
+              <h1>Como Funciona</h1>
+              <ul className="list-about">
+                <li>Digite ou cole seu o link no campo.</li>
+                <li>Aponte sua camera para testar o QR Code.</li>
+                <li>
+                  Clique no Botão direito na imagem clique em copiar ou baixar
+                  imagem, e pronto.
+                </li>
+              </ul>
+              <p>
+                O gerador de QR Code é uma ótima ferramenta para ações de
+                marketing, relacionamento. Com o gerador de QR Codes você tem a
+                possibilidade de utilizar em campanhar de marketing digital.
+                Como a ferramenta disponibiliza o png para download, facilita
+                muito na hora da criação da sua campanha. Utilize com moderação
+                rsrs.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
